79b46fd17262c310578386d9273a5e9c785d0c8a
Peter Anderson <pete.anderson@gmail.com>
2018-12-16 21:31:01 +0100

    Added a reset and display the winning lunch option

--- src/search.js
+++ src/search.js
@@ -8,7 +8,6 @@ if (process.env.NODE_ENV === 'production') {
     secrets = require('./secrets');
 }
 
-
 let col1 = 0;
 let col2 = 0;
 let col3 = 0;
@@ -18,6 +17,7 @@ export class Search extends React.Component {
         super(props);
         this.state = {
             picked: false,
+            buttonDisabled: true,
             participants: [
                 {id: 1},
                 {id: 2},
@@ -30,39 +30,86 @@ export class Search extends React.Component {
         this.handleVote = this.handleVote.bind(this);
         this.addParticipant = this.addParticipant.bind(this);
         this.checkWinner = this.checkWinner.bind(this);
+        this.reset = this.reset.bind(this);
         let address;
     }
     handleSearchInput(e) {
         this.address = e.target.value;
-        console.log(this.address);
-
+        if (this.address.length >= 1) {
+            this.setState({ buttonDisabled: false })
+        }
     }
     handleClick() {
         const qs = `https://api.foursquare.com/v2/venues/explore?client_id=${secrets.CLIENT_ID}&client_secret=${secrets.CLIENT_SECRET}&query=lunch&near=${this.address}&v=20170801&limit=3`
-        console.log(qs);
         axios.get(qs)
             .then(data => {
                 console.log(data.data.response.groups[0].items);
                 this.setState({ searchResultsArr: data.data.response.groups[0].items });
+                this.reset();
             }).catch(err => { console.log(err); });
     }
 
     handleVote(e) {
         console.log("clicked! ", e);
+        if (e.classList.contains('1') && e.classList.contains('voting')) {
+            col1++;
+        } else if (e.classList.contains('2')  && e.classList.contains('voting')) {
+            col2++;
+        } else if (e.classList.contains('3')  && e.classList.contains('voting')) {
+            col3++;
+        }
+        e.classList.remove("voting");
         e.classList.add("picked");
         this.checkWinner(e);
     }
     checkWinner(e) {
         console.log("checked!", e);
-        if (e.classList.contains('1')) {
-            col1++;
-        } else if (e.classList.contains('2')) {
-            col2++;
-        } else if (e.classList.contains('3')) {
-            col3++;
+        if (col1 > 1 || col2 > 1 || col3 > 1) {
+            if (col1 > col2 && col1 > col3) {
+                let winner = document.getElementsByClassName('restaurant0');
+                winner[0].classList.add('winner');
+                document.getElementsByClassName('restaurant1')[0].classList.remove('winner');
+                document.getElementsByClassName('restaurant2')[0].classList.remove('winner');
+            } else if (col2 > col1 && col2 > col3) {
+                let winner = document.getElementsByClassName('restaurant1');
+                winner[0].classList.add('winner');
+                document.getElementsByClassName('restaurant0')[0].classList.remove('winner');
+                document.getElementsByClassName('restaurant2')[0].classList.remove('winner');
+            } else if (col3 > col1 && col3 > col1) {
+                let winner = document.getElementsByClassName('restaurant2');
+                winner[0].classList.add('winner');
+                document.getElementsByClassName('restaurant0')[0].classList.remove('winner');
+                document.getElementsByClassName('restaurant1')[0].classList.remove('winner');
+            }
         }
-        console.log(col1, col2, col3);
 
+    }
+    reset() {
+        col1 = 0;
+        col2 = 0;
+        col3 = 0;
+        this.setState({
+            participants: [
+                {id: 1},
+                {id: 2},
+                {id: 3},
+                {id: 4}
+            ]
+        });
+        console.log("reset all the votes");
+        let box = document.getElementsByClassName('box');
+        for (let i = 0; i < box.length; i++) {
+            box[i].classList.add('voting');
+            box[i].classList.remove('picked');
+        }
+        let winnersToRemove = document.getElementsByClassName('single-search-result');
+        for (let i = 0; i < winnersToRemove.length; i++) {
+            winnersToRemove[i].classList.remove('winner');
+        }
+        let inputFields = document.getElementsByClassName('userName');
+        for (let j= 0; j < inputFields.length; j++) {
+            inputFields[j].value = "";
+        }
     }
     addParticipant() {
         let oldId = this.state.participants[this.state.participants.length - 1].id;
@@ -80,18 +127,18 @@ export class Search extends React.Component {
             return (
                 <div className="searchmodal">
                     <input id="search" name="search" type="text" placeholder="search for a place to eat"  onChange={ this.handleSearchInput }  />
-                    <button className="btn" onClick={ this.handleClick } >Search</button>
+                    <button disabled={this.state.buttonDisabled} className="btn" onClick={ this.handleClick } >Search</button>
                 </div>
             );
         } else {
             return (
                 <div className="searchmodal">
                     <input id="search" name="search" type="text" placeholder="search for a place to eat" onChange={ this.handleSearchInput }   />
-                    <button className="btn" onClick={ this.handleClick } >Search</button>
+                    <button disabled={this.state.buttonDisabled} className="btn" onClick={ this.handleClick } >Search</button>
                     <div className="search-result">
                         <div className="single-search-result blank"></div>
                         { this.state.searchResultsArr.map( (venue, idx) => (
-                                <div className="single-search-result" key={venue.venue.id}>
+                                <div className={'single-search-result restaurant' + idx} key={venue.venue.id}>
                                     <a href={`https://www.google.com/search?q=${venue.venue.name}+${this.address}`} >
                                         <h3>{venue.venue.name} </h3>
                                     </a>
@@ -104,11 +151,11 @@ export class Search extends React.Component {
                             return(
                                 <React.Fragment key={idx}>
                                     <div className="voting">
-                                    <input id="addname" name="search" type="text" placeholder="Add your name"  onChange={ this.handleSearchInput }  />
+                                    <input className="userName" id="addname" name="search" type="text" placeholder="Add your name"  onChange={ this.handleSearchInput }  />
                                     </div>
-                                    <div className={this.state.picked ? 'picked 1': 'voting 1'} id={idx +1} onClick={(e) => this.handleVote(e.target)}></div>
-                                    <div className={this.state.picked ? 'picked 2': 'voting 2'} id={idx +1} onClick={(e) => this.handleVote(e.target)}></div>
-                                    <div className={this.state.picked ? 'picked 3': 'voting 3'} id={idx +1}  onClick={(e) => this.handleVote(e.target)}></div>
+                                    <div className={this.state.picked ? 'box picked 1': 'box voting 1'} id={idx +1} onClick={(e) => this.handleVote(e.target)}></div>
+                                    <div className={this.state.picked ? 'box picked 2': 'box voting 2'} id={idx +1} onClick={(e) => this.handleVote(e.target)}></div>
+                                    <div className={this.state.picked ? 'box picked 3': 'box voting 3'} id={idx +1}  onClick={(e) => this.handleVote(e.target)}></div>
                                 </React.Fragment>
                             )
 
